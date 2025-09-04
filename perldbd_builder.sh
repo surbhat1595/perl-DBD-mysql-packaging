64c570c870ceebb5380b6c9c5b7e302f72008555
Vadim Yalovets <vadim.yalovets@percona.com>
2025-09-04 13:22:14 +0300

    RM-1691 PDPS 8.4.6-6 release

--- perldbd_builder.sh
+++ perldbd_builder.sh
@@ -134,6 +134,7 @@ get_sources(){
     VERSION_TMP=$(echo ${VERSION}| sed -e 's:_:.:')
     sed -i "s|^\(Version:\s*\).*|\1${VERSION_TMP}|" rpm/perl-DBD-MySQL.spec
     sed -i "s|Release:        [1-9]|Release:        ${RPM_RELEASE}|g" rpm/perl-DBD-MySQL.spec
+    sed -i "s|^\(%setup -q -n %{name}-\)[0-9A-Za-z._-]*|\1${VERSION}|" rpm/perl-DBD-MySQL.spec
     cd ..
     cp -r packaging/debian ./
     
